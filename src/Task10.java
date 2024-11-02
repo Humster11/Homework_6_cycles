c0b46e55175333b7753b1d481d7bd712b9fb765c
daria.lapina <ms.humster11@gmail.com>
2024-11-03 02:27:48 +0300

    Исправленное ДЗ

--- src/Task10.java
+++ src/Task10.java
@@ -2,8 +2,8 @@ public class Task10 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             int number = 2;
-            int multiplaction = number*i;
-            System.out.println(number+" * "+i+" = "+multiplaction);
+            int multiplaction = number * i;
+            System.out.println(number + " * " + i + " = " + multiplaction);
         }
     }
 }
